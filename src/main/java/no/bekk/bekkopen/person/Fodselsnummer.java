b5ffc950ec7f2dd3be63961198de79d4934c576f
Lars Erik Storbukås <larserik.storbukas@gmail.com>
2021-02-04 08:13:23 +0100

    Revert "Støtt syntetiske fnr og dnr for folkeregisteret test"
    This reverts commit ededdc311826188eaf6e36107f1bf8fdd9b9d340.

--- src/main/java/no/bekk/bekkopen/person/Fodselsnummer.java
+++ src/main/java/no/bekk/bekkopen/person/Fodselsnummer.java
@@ -21,7 +21,7 @@ public class Fodselsnummer extends StringNumber {
 	 * @return A String containing the date and month of birth.
 	 */
 	public String getDateAndMonth() {
-		return parseSynthenticNumber(parseDNumber(getValue())).substring(0, 4);
+		return parseDNumber(getValue()).substring(0, 4);
 	}
 
 	/**
@@ -31,7 +31,7 @@ public class Fodselsnummer extends StringNumber {
 	 * @return A String containing the date of birth
 	 */
 	public String getDayInMonth() {
-		return parseSynthenticNumber(parseDNumber(getValue())).substring(0, 2);
+		return parseDNumber(getValue()).substring(0, 2);
 	}
 
 	/**
@@ -41,7 +41,7 @@ public class Fodselsnummer extends StringNumber {
 	 * @return A String containing the date of birth
 	 */
 	public String getMonth() {
-		return parseSynthenticNumber(parseDNumber(getValue())).substring(2, 4);
+		return parseDNumber(getValue()).substring(2, 4);
 	}
 
 	/**
@@ -86,7 +86,7 @@ public class Fodselsnummer extends StringNumber {
 	 * @return A String containing the date and month of birth.
 	 */
 	public String getDateOfBirth() {
-		return parseSynthenticNumber(parseDNumber(getValue())).substring(0, 6);
+		return parseDNumber(getValue()).substring(0, 6);
 	}
 
 	/**
@@ -155,18 +155,10 @@ public class Fodselsnummer extends StringNumber {
 		return !isMale();
 	}
 
-  static String parseSynthenticNumber(String fodselsnummer) {
-    if (!isSynthetic(fodselsnummer)) {
-      return fodselsnummer;
-    } else {
-      return fodselsnummer.substring(0, 2) + (getThirdDigit(fodselsnummer) - 8) + fodselsnummer.substring(3);
-    }
-  }
-
-  static boolean isSynthetic(String fodselsnummer) {
+	static boolean isDNumber(String fodselsnummer) {
 		try {
-			int thirdDigit = getThirdDigit(fodselsnummer);
-			if (thirdDigit == 8 || thirdDigit == 9) {
+			int firstDigit = getFirstDigit(fodselsnummer);
+			if (firstDigit > 3 && firstDigit < 8) {
 				return true;
 			}
 		} catch (IllegalArgumentException e) {
@@ -174,18 +166,6 @@ public class Fodselsnummer extends StringNumber {
 		}
 		return false;
 	}
-
-  static boolean isDNumber(String fodselsnummer) {
-    try {
-      int firstDigit = getFirstDigit(fodselsnummer);
-      if (firstDigit > 3 && firstDigit < 8) {
-        return true;
-      }
-    } catch (IllegalArgumentException e) {
-      // ignore
-    }
-    return false;
-  }
 
 	static String parseDNumber(String fodselsnummer) {
 		if (!isDNumber(fodselsnummer)) {
@@ -197,10 +177,6 @@ public class Fodselsnummer extends StringNumber {
 
 	private static int getFirstDigit(String fodselsnummer) {
 		return Integer.parseInt(fodselsnummer.substring(0, 1));
-	}
-
-	private static int getThirdDigit(String fodselsnummer) {
-		return Integer.parseInt(fodselsnummer.substring(2, 3));
 	}
 
 	public KJONN getKjonn() {
